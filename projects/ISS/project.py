bc4812bbbfdd01eaeb7e7fcaad8f6a4b9e0c0787
Dominik Křivohlávek <domkrivohlavek@gmail.com>
2017-11-20 21:02:24 +0100

    Opening and closing input

--- projects/ISS/project.py
+++ projects/ISS/project.py
@@ -2,3 +2,15 @@
 
 # Python 3.6 script for ISS project
 # Author: Dominik Křivohlávek <xkrivo02>
+
+import sys
+import wave
+# pylint: disable-msg=C0103
+
+# Open passed file for analysis
+inputFile = wave.open(sys.argv[1], "rb")
+
+# TODO: check for errors
+
+# Finally, close file
+inputFile.close()
