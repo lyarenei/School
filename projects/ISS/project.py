bd2606cdef75b36bda01e338ea73e79e4e22c46f
lyarenei <domkrivohlavek@gmail.com>
2017-11-23 00:31:18 +0100

    Backup push, will be reset

--- projects/ISS/project.py
+++ projects/ISS/project.py
@@ -13,73 +13,124 @@ from scipy.io import wavfile
 
 # pylint: disable-msg=C0103
 
-# Open passed file for analysis
-inputFile = wave.open(sys.argv[1], "rb")
 
-# TODO: check for errors
+def GetParams(filename):
+    "Gets wav file parameters"
+    inputFile = wave.open(filename, "rb")
+    # TODO: check for open error if needed
+    inputParams = inputFile.getparams()
+    inputFile.close()
+    return inputParams
 
-# Fetch input file parameters
-inputParams = inputFile.getparams()
+
+def PlotGraph(x_axis, y_axis, x_label, y_label, title):
+    "Plots and shows graph"
+    # TODO: Add labels to graph
+    gplot.plot(x_axis, y_axis)
+    gplot.show()
+
+
+def task_1(filename):
+    "Takes care of Task 1"
+    inputParams = GetParams(filename)
+    print("Task 1:")
+    print("\tSampling frequency:", inputParams.framerate, "Hz")
+    print("\tNumber of samples:", inputParams.nframes)
+    print("\tTime: ", inputParams.framerate / inputParams.nframes, "s")
+
+
+def task_2(filename):
+    "Takes care of Task 2"
+    inputParams = GetParams(filename)
+    sampleRate, inputData = wavfile.read(filename)
+
+    signal_len = inputParams.framerate / inputParams.nframes
+
+    # Get all samples into x_input variable
+    x_input = linspace(0.0, inputParams.nframes *
+                       signal_len, inputParams.nframes)
+
+    # Evaluate fourier coefficients (and normalize them)
+    spectrum_coeff = fftpack.fft(inputData) / inputParams.nframes
+
+    # Plot frequency spectrum
+    print("Task 2: \n\tPlotting frequency spectrum...")
+    PlotGraph(x_input[range(0, int(inputParams.nframes / 2))],
+              abs(spectrum_coeff)[range(0, int(inputParams.nframes / 2))], "frequency [Hz]", "module", "Task 2")
+
+
+def task_3(filename):
+    "Takes care of Task 3"
+    inputParams = GetParams(filename)
+    sampleRate, inputData = wavfile.read(filename)
+    spectrum_coeff = fftpack.fft(inputData) / inputParams.nframes
+    print("Task 3: \n\tModule maximum is on:", argmax(spectrum_coeff), "Hz")
+
+
+def task_4():
+    "Takes care of Task 4"
+
+
+def task_5():
+    "Takes care of Task 5"
+
+
+def task_6(filename, a_iir_coeff, b_iir_coeff):
+    "Takes care of Task 6"
+    zi = signal.lfiltic(a_iir_coeff, b_iir_coeff, y=[2.])
+
+    inputParams = GetParams(filename)
+    sampleRate, inputData = wavfile.read(filename)
+
+    signal_len = inputParams.framerate / inputParams.nframes
+
+    filtered_signal = signal.lfilter(
+        a_iir_coeff, b_iir_coeff, inputData, zi=zi)
+
+    filtered_spectrum = fftpack.fft(filtered_signal[0]) / inputParams.nframes
+
+    # Get all samples into x_input variable
+    x_input = linspace(0.0, inputParams.nframes *
+                       signal_len, inputParams.nframes)
+
+    PlotGraph(x_input[range(0, int(inputParams.nframes / 2))], abs(filtered_spectrum)
+              [range(0, int(inputParams.nframes / 2))], "frequency [Hz]", "module", "Task 6")
+
+
+def task_7(filename, a_iir_coeff, b_iir_coeff):
+    "Takes care of Task 7"
+    zi = signal.lfiltic(a_iir_coeff, b_iir_coeff, y=[2.])
+
+    inputParams = GetParams(filename)
+    sampleRate, inputData = wavfile.read(filename)
+
+    filtered_signal = signal.lfilter(
+        a_iir_coeff, b_iir_coeff, inputData, zi=zi)
+
+    filtered_spectrum = fftpack.fft(filtered_signal[0]) / inputParams.nframes
+    print("Task 7: \n\tModule maximum in filtered signal is on:",
+          argmax(filtered_spectrum), "Hz")
 
 
 # [Task 1] Write down sampling frequency, length in samples and time:
-print("Task 1:")
-print("\tSampling frequency:", inputParams.framerate, "Hz")
-print("\tNumber of samples:", inputParams.nframes)
-print("\tTime: ", inputParams.framerate / inputParams.nframes, "s")
-
+    task_1(sys.argv[1])
 
 # [Task 2] Evaluate signal spectrum with DFT, plot spectrum module up to F_s / 2 frequency:
-
-# Read input data
-sampleRate, inputData = wavfile.read(sys.argv[1])
-
-samples = inputParams.nframes
-length = inputParams.framerate / inputParams.nframes
-
-# Get all samples into x_input variable
-x_input = linspace(0.0, samples * length, samples)
-
-# Evaluate fourier coefficients (and normalize them)
-dft_coefficients = fftpack.fft(inputData) / samples
-
-# Plot frequency spectrum
-print("Task 2: \n\tPlotting frequency spectrum...")
-# TODO: Add labels to graph
-gplot.plot(x_input[range(0, int(samples / 2))],
-           abs(dft_coefficients)[range(0, int(samples / 2))])
-gplot.show()
-
+    task_2(sys.argv[1])
 
 # [Task 3] Evaluate on which frequency is the maximum module value
-print("Task 3: \n\tModule maximum is on:", argmax(dft_coefficients), "Hz")
-
+    task_3(sys.argv[1])
 
 # [Task 4] Given IIR filter, evaluate and plot poles and zeroes of its transfer function
-
+    task_4()
 
 # [Task 5] Plot frequency module of the IIR filter
-
+    task_5()
 
 # [Task 6] Filter input signal through IIR filter, plot frequency spectrum up to F_s / 2
-a_iir_coeff = array([1.0, 0.2289, 0.4662])
-b_iir_coeff = array([1.0, -0.2324, 0.4112, -0.2324])
-
-zi = signal.lfiltic(a_iir_coeff, b_iir_coeff, y=[2.])
-
-filtered_signal = signal.lfilter(a_iir_coeff, b_iir_coeff, inputData, zi=zi)
-
-filtered_spectrum = fftpack.fft(filtered_signal[0]) / samples
-
-gplot.plot(x_input[range(0, int(samples / 2))],
-           abs(filtered_spectrum)[range(0, int(samples / 2))])
-gplot.show()
-
+    a_iir_coeff = array([1.0, 0.2289, 0.4662])
+    b_iir_coeff = array([1.0, -0.2324, 0.4112, -0.2324])
+    task_6(sys.argv[1], a_iir_coeff, b_iir_coeff)
 
 # [Task 7] Evaluate on which frequency is the maximum module value in filtered signal
-print("Task 7: \n\tModule maximum in filtered signal is on:",
-      argmax(filtered_spectrum), "Hz")
-
-
-# Finally, close file
-inputFile.close()
+    task_7(sys.argv[1], a_iir_coeff, b_iir_coeff)
