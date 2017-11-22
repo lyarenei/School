b50948f348102f2833ab45056ff9c98ac1de8f6e
lyarenei <domkrivohlavek@gmail.com>
2017-11-22 23:31:59 +0100

    Task 6 implementation

--- projects/ISS/project.py
+++ projects/ISS/project.py
@@ -6,8 +6,9 @@
 import sys
 import wave
 import matplotlib.pyplot as gplot
-from numpy import linspace, argmax
+from numpy import linspace, argmax, array, ndarray
 from scipy import fftpack
+from scipy import signal as signal
 from scipy.io import wavfile
 
 # pylint: disable-msg=C0103
@@ -37,7 +38,7 @@ samples = inputParams.nframes
 length = inputParams.framerate / inputParams.nframes
 
 # Get all samples into x_input variable
-x_input = linspace(0.0, samples*length, samples)
+x_input = linspace(0.0, samples * length, samples)
 
 # Evaluate fourier coefficients (and normalize them)
 dft_coefficients = fftpack.fft(inputData) / samples
@@ -45,12 +46,34 @@ dft_coefficients = fftpack.fft(inputData) / samples
 # Plot frequency spectrum
 print("Task 2: \n\tPlotting frequency spectrum...")
 # TODO: Add labels to graph
-gplot.plot(x_input[range(0, int(samples / 2))], abs(dft_coefficients)[range(0, int(samples / 2))])
+gplot.plot(x_input[range(0, int(samples / 2))],
+           abs(dft_coefficients)[range(0, int(samples / 2))])
 gplot.show()
 
 
 # [Task 3] Evaluate on which frequency is the maximum module value
 print("Task 3: \n\tModule maximum is on:", argmax(dft_coefficients), "Hz")
+
+
+# [Task 4] Given IIR filter, evaluate and plot poles and zeroes of its transfer function
+
+
+# [Task 5] Plot frequency module of the IIR filter
+
+
+# [Task 6] Filter input signal through IIR filter, plot frequency spectrum up to F_s / 2
+a_iir_coeff = array([1.0, 0.2289, 0.4662])
+b_iir_coeff = array([1.0, -0.2324, 0.4112, -0.2324])
+
+zi = signal.lfiltic(a_iir_coeff, b_iir_coeff, y=[2.])
+
+filtered_signal = signal.lfilter(a_iir_coeff, b_iir_coeff, inputData, zi=zi)
+
+filtered_spectrum = fftpack.fft(filtered_signal[0]) / samples
+
+gplot.plot(x_input[range(0, int(samples / 2))],
+           abs(filtered_spectrum)[range(0, int(samples / 2))])
+gplot.show()
 
 
 # Finally, close file
