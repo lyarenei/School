4be95339bee605b088d6799379a7f880d70ca557
Dominik Křivohlávek <domkrivohlavek@gmail.com>
2018-02-07 12:21:39 +0100

    InsertInstruction method

--- projects/IPP/src/Program.php
+++ projects/IPP/src/Program.php
@@ -10,6 +10,17 @@
         public function SetProgramLanguage($language) {
             $this->language = $language;
         }
+
+        public function InsertInstruction($instructionLine) {
+            static $counter = 1;
+            $instruction = new Instruction();
+            $instructionParams = preg_split('/\s+/', $instructionLine);
+            $instruction->SetOrder($counter);
+            $instruction->SetOpcode($instructionParams[0]);
+            $instruction->SetInstructionArguments($instructionParams);
+            array_push($this->instructions, $instruction);
+            $counter++;
+        }
     }
 
 ?>
