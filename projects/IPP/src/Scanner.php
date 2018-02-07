44df1ef9b37cc4fb5cd25ef4bbed9e6da50f3222
Dominik Křivohlávek <domkrivohlavek@gmail.com>
2018-02-07 12:26:26 +0100

    GetLine and ParseLine methods

--- projects/IPP/src/Scanner.php
+++ projects/IPP/src/Scanner.php
@@ -12,6 +12,15 @@
             $this->logger = new Logger();
         }
 
+        private function ParseLine($line) {
+            $trimmedLine = trim($line, " \t\n\r\0\x0B");
+            $opcode = explode('#', $trimmedLine);
+            if (empty($opcode[0])) {
+                return FALSE;
+            }
+            return $opcode[0];
+        }
+
         public function OpenFile($fileName) {
             if (is_readable($fileName)) {
                 $this->sourceFile = fopen($fileName, "r");
@@ -19,6 +28,18 @@
             else {
                 $this->logger->PrintError("Cannot open file: The specified file does not exist.", 1);
             }
+        }
+
+        public function GetLine() {
+            do {
+                $line = fgets($this->sourceFile);
+                $opcode = $this->ParseLine($line);
+                if ($opcode == FALSE || empty($opcode)) {
+                    continue;
+                } else {
+                    return $opcode;
+                }
+            } while ($line != FALSE);
         }
     }
 
