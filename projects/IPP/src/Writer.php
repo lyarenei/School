0e81fef0087d1fd050f0dfc477e91db5e11f6e87
Dominik Křivohlávek <domkrivohlavek@gmail.com>
2018-02-08 01:05:32 +0100

    Write method

--- projects/IPP/src/Writer.php
+++ projects/IPP/src/Writer.php
@@ -1,16 +1,30 @@
 <?php
 
-    class Writer {
-        private $outputFile;
-        private $xmlWriter;
+class Writer {
+    private $__logger;
+    private $__xmlWriter;
 
-        private function __construct() {
-            $xmlWriter = new XMLWriter();
-        }
-
-        public function Write($outputFile) {
-            // TBD
-        }
+    public function __construct() {
+        $this->__logger = new Logger();
+        $this->__xmlWriter = new XMLWriter();
     }
+
+    private function __WriteProgram($parsedProgram) {
+
+    }
+
+    public function Write($parsedProgram, $outputFile) {
+        $this->__xmlWriter->openMemory();
+        $this->__xmlWriter->setIndent(4);
+        $this->__xmlWriter->setIndentString(' ');
+        $this->__xmlWriter->startDocument('1.0', 'UTF-8');
+
+        $this->__WriteProgram($parsedProgram);
+
+        $this->__xmlWriter->endDocument();
+        // TODO: refactor into method to write to any file
+        echo $this->__xmlWriter->outputMemory();
+    }
+}
 
 ?>
