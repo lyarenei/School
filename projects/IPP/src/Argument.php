9b482cebfa4f551e35b50bdab83ba78220b60aee
Dominik Křivohlávek <domkrivohlavek@gmail.com>
2018-02-08 21:11:41 +0100

    Added basic argument parsing method

--- projects/IPP/src/Argument.php
+++ projects/IPP/src/Argument.php
@@ -28,6 +28,31 @@ class Argument {
     public function getOrder() {
         return $this->__order;
     }
+
+    public function parseArgument($argument) {
+        $type = explode('@', $argument)[0];
+
+        switch ($type) {
+        case 'int':
+        case 'bool':
+        case 'string':
+            $this->__type = $type;
+            $this->__value = preg_split('/.*@/i', $argument)[1];
+            break;
+
+        case 'GF':
+        case 'LF':
+        case 'TF':
+            $this->__type = 'var';
+            $this->__value = $argument;
+            break;
+
+        default:
+            $this->__type = 'label';
+            $this->__value = $argument;
+        }
+        return $argument;
+    }
 }
 
 ?>
